64cf3ac94eaa30860a4f218d74e71b6d04f7c7c5
Ihtisham Saeed <111792380+ihtishamsaeed414@users.noreply.github.com>
2024-07-31 10:45:55 +0500

    performance optimization completed

--- src/PostProvider.js
+++ src/PostProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -32,15 +32,27 @@ export function PostProvider({ children }) {
   function handleClearPosts() {
     setPosts([]);
   }
+
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+      onAddPost: handleAddPost,
+    };
+  }, [searchedPosts, searchQuery]);
+
   return (
     <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-        onAddPost: handleAddPost,
-      }}
+      // value={{
+      //   posts: searchedPosts,
+      //   onClearPosts: handleClearPosts,
+      //   searchQuery,
+      //   setSearchQuery,
+      //   onAddPost: handleAddPost,
+      // }}
+      value={value}
     >
       {children}
     </PostContext.Provider>
